61589e7fb4940ddabae271143ca151492096867f
Fiza Ali <140485687+Fizaali9474@users.noreply.github.com>
2024-03-28 06:35:47 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,190 @@
+
+//dropdown/
+
+/*var arr=['iphone','samsung','motorola','infinix']
+document.write(
+'<select>'
+)
+
+for(var i=0;i<arr.length;i++)
+document.write('<option>'+arr[i] ,'<br>' )
+document.write('</select>')
+
+//countings//
+
+//1//
+
+
+
+for (var i=1;i<=5;i++)   
+
+{
+for (var j=1;j<=i;j++)
+{
+document.write(j+ "")
+
+}
+document.write('<br>')
+}
+
+
+
+
+//2//
+
+{
+    document.write('<br>')
+
+}
+         for( var i=1; i<=5; i++  ){  
+            var num='';
+            document.write('<br>')
+                     for(var j=1; j<=5 ; j++)    {
+        num+=j+'';
+            
+            }
+            
+            document.write(num);
+        }
+
+{
+    document.write('<br>')
+}
+//3//
+
+{
+    document.write('<br>')
+}
+
+  for (var i=5;i>=1;i--)   
+
+{
+for (var j=5;j>=i;j--)
+{
+document.write(j+ "")
+
+}
+document.write('<br>')
+}
+{
+document.write('<br>')
+}
+
+
+//4/
+
+
+for (var i=1;i<=5;i++)   {
+   var num='';
+document.write('<br>')
+{
+for (var j=1;j<=i;j++)
+{
+num+=i+ ''
+
+}
+document.write(num)
+}
+}
+ {
+    document.write('<br>')
+ }
+
+//5//
+
+for (var i = 4; i <= 8; i++) {
+  var   num = '';
+  document.write('<br>')
+    for (var j = i; j >= 4; j--) {
+      num += j + ' ';
+    }
+    document.write(num);
+  }
+  
+
+{
+    document.write('<br> ')
+}
+
+//6//
+    
+{
+    document.write('<br>')
+}
+
+for (var i=8;i>=4;i--)   
+
+{
+for (var j=8;j>=i;j--)
+{
+document.write(j+ "")
+
+}
+document.write('<br>')
+
+}
+
+var user=prompt ('Enter a city')
+var arr=['Multan','Lahore','Karachi','Peshawar','Quetta']
+var first=user.slice(0,1).toUpperCase()
+var second=user.slice(1).toLowerCase()
+var final=first+second
+document.write(final)
+for (var i=0;i<arr.length;i++)
+{ if (user===arr[i])
+alert('You selected'+arr [0]   +  'Speciality is Halwa'  )
+{
+  document.write('<br>')
+}
+}
+
+
+
+for (let i = 0; i < 5; i++) {
+
+  document.write('Hellow World' + '<br>')
+}
+
+
+for (let i = 1; i <= 10; i++) {
+  document.write(i,'<br>'); 
+}
+
+
+
+
+var table=  prompt("Enter the table number ")
+
+
+  for ( i = 1; i <=10; i++) {
+    document.write(table +'x'+ i + '='+table*i+'<br>')
+  }
+
+
+
+var arr=['Nokia' , 'Samsung' , 'Apple' , 'Sony' , 'Huawei']
+for(i=0; i<arr.length ; i++)
+{document.write(arr[i]+'<br>')}
+
+
+
+
+var arr = ['Apple', 'Banana' , 'Mango' , 'Orange' , 'Strawberry'];
+
+for (i = 0; i <1; i++) {
+   document.write(arr)
+
+}
+
+var num=prompt('Enter a number')
+
+var index0=prompt('Enter value for index 0')
+
+var index1=prompt('Enter value for index 1')
+
+var index2=prompt('Enter value for index 2')
+
+var index3=prompt('Enter value for index 3')
+
+
+document.write(index0 , index1 , index2 , index3 + '<br>')*/
